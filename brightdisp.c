9cd212e584444290ce49d8ba0973ce151423aadb
Shashanov Andrey <shashanov@outlook.com>
2019-06-05 00:14:46 +0300

    Add files via upload

--- brightdisp.c
+++ brightdisp.c
@@ -19,10 +19,10 @@ SUBSYSTEM=="backlight", ACTION=="add", \
 
 */
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "build_host.h"
 
